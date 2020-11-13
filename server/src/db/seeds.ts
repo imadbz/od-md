32210fd3ee7fd8a1a6b40cc86cea927de754cbd2
Imad Bouziani <oumdadzn@gmail.com>
2020-11-13 12:11:24 +0800

    add user search support

--- server/src/db/seeds.ts
+++ server/src/db/seeds.ts
@@ -3,7 +3,7 @@ import { getConnection } from 'typeorm'
 import faker from 'faker';
 import { User } from "../entities/User";
 
-const users = new Array(50).fill(null)
+const users = new Array(100).fill(null)
   .map((user, index) =>
     user = { name: `${faker.name.firstName()} ${faker.name.lastName()}`, shortBio: faker.lorem.words(7), isVerified: faker.random.boolean(), imageUrl: `https://i.pravatar.cc?u=${index}` }
   )
--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -4,8 +4,20 @@ import { User } from "../entities/User";
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
-  Users() {
-    return User.find();
+  Users(
+    @Arg("skip") skip: number,
+    @Arg("take") take: number,
+    @Arg("search", { nullable: true }) search: string,
+  ) {
+
+    if (search) {
+      return User.createQueryBuilder()
+        .select()
+        .where("user.name like :search or user.shortBio like :search", { search })
+        .getMany();
+    }
+
+    return User.find({ skip, take });
   }
 
   @Query(() => User)
